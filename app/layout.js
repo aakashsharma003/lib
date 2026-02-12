d82dc7f9a874132a6711a1b5376c03765b61d312
Akash Sharma <122847899+aakashsharma003@users.noreply.github.com>
2026-02-12 15:33:37 +0530

    Integrate web tracking analytics script
    Added a Script component for web tracking analytics.

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Header } from "@/components/header";
 import ClerkProviderWrapper from "@/components/clerk-provider-wrapper";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,13 @@ export default function RootLayout({ children }) {
         <ClerkProviderWrapper>
           {children}
         </ClerkProviderWrapper>
+
+        <Script 
+          src="https://www.webtracky.com/analytics.js"
+          data-tracker="036ac35e-9bb5-4253-938b-fc0742846433"
+          data-hosts="join-library.vercel.app"
+          strategy="afterInteractive" 
+        />
       </body>
     </html>
   );
